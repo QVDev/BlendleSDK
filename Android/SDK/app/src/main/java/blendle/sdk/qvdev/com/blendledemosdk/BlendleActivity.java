6896a5c1a8b669a05493244efbf17fa8559b1711
QVDev <feedbackandroid@gmail.com>
2015-10-03 17:15:34 +0200

    Improved user image display

--- Android/SDK/app/src/main/java/blendle/sdk/qvdev/com/blendledemosdk/BlendleActivity.java
+++ Android/SDK/app/src/main/java/blendle/sdk/qvdev/com/blendledemosdk/BlendleActivity.java
@@ -54,18 +54,23 @@ public class BlendleActivity extends AppCompatActivity
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getUser();
+            }
+        };
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
         initInfo();
     }
 
     private void initInfo() {
 //        getBlendleApi();
-        getUser();
     }
 
     private void getBlendleApi() {
@@ -98,8 +103,6 @@ public class BlendleActivity extends AppCompatActivity
                         .load(userResponse.getLinks().getLargeAvatar().getHref())
                         .transform(new CircleTransform(BlendleActivity.this))
                         .into(userImage);
-
-
             }
 
             @Override
--- Android/SDK/app/src/main/java/blendle/sdk/qvdev/com/blendledemosdk/CircleTransform.java
+++ Android/SDK/app/src/main/java/blendle/sdk/qvdev/com/blendledemosdk/CircleTransform.java
@@ -15,14 +15,12 @@ public class CircleTransform extends BitmapTransformation {
     }
 
     @Override
-    protected Bitmap transform(BitmapPool pool, Bitmap toTransform,
-                               int outWidth, int outHeight) {
+    protected Bitmap transform(BitmapPool pool, Bitmap toTransform, int outWidth, int outHeight) {
         return circleCrop(pool, toTransform);
     }
 
     private static Bitmap circleCrop(BitmapPool pool, Bitmap source) {
-        if (source == null)
-            return null;
+        if (source == null) return null;
 
         int size = Math.min(source.getWidth(), source.getHeight());
         int x = (source.getWidth() - size) / 2;
@@ -33,14 +31,12 @@ public class CircleTransform extends BitmapTransformation {
 
         Bitmap result = pool.get(size, size, Bitmap.Config.ARGB_8888);
         if (result == null) {
-            result = Bitmap.createBitmap(size, size,
-                    Bitmap.Config.ARGB_8888);
+            result = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888);
         }
 
         Canvas canvas = new Canvas(result);
         Paint paint = new Paint();
-        paint.setShader(new BitmapShader(squared,
-                BitmapShader.TileMode.CLAMP, BitmapShader.TileMode.CLAMP));
+        paint.setShader(new BitmapShader(squared, BitmapShader.TileMode.CLAMP, BitmapShader.TileMode.CLAMP));
         paint.setAntiAlias(true);
         float r = size / 2f;
         canvas.drawCircle(r, r, r, paint);
