cc72118c9135e0c69c6bfdd030351a8034ac49fe
QVDev <feedbackandroid@gmail.com>
2015-10-15 20:48:59 +0200

    Implement getting next article items based on url

--- Android/SDK/blendleandroidsdk/src/main/java/com/sdk/Example.java
+++ Android/SDK/blendleandroidsdk/src/main/java/com/sdk/Example.java
@@ -1,6 +1,7 @@
 package com.sdk;
 
 import com.sdk.blendle.models.generated.login.Login;
+import com.sdk.blendle.models.generated.search.Search;
 
 import retrofit.Callback;
 import retrofit.Response;
@@ -8,10 +9,32 @@ import retrofit.Retrofit;
 
 public class Example {
 
-    public static void main(String args[]) {
-        BlendleApi blendleApi = new BlendleApi();
+    private static BlendleApi mBlendleApi;
 
-        blendleApi.loginUser(new Callback<Login>() {
+    public static void main(String args[]) {
+        mBlendleApi = new BlendleApi();
+
+//        loginUser("username", "password");
+        loadNextArticles("https://ws.blendle.nl/search?q=blendle&limit=10&offset=10");
+    }
+
+    private static void loadNextArticles(String nextLink) {
+        mBlendleApi.loadNextArticles(new Callback<Search>() {
+            @Override
+            public void onResponse(Response<Search> response, Retrofit retrofit) {
+                Search nextArticles = response.body();
+                System.out.println(nextArticles.getResults());
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                System.out.println(t.getMessage());
+            }
+        }, nextLink);
+    }
+
+    private static void loginUser(String username, String password) {
+        mBlendleApi.loginUser(new Callback<Login>() {
             @Override
             public void onResponse(Response<Login> response, Retrofit retrofit) {
                 if (response.isSuccess()) {
@@ -26,6 +49,6 @@ public class Example {
             public void onFailure(Throwable t) {
                 System.out.println(t.getMessage());
             }
-        }, "username", "password");
+        }, username, password);
     }
 }
